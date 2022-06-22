fc029df16421185f3da0d2f230bb825e88e55be1
SheshkoPavel <derzurb@mail.ru>
2022-06-23 01:32:00 +0300

    feat: task completed

--- src/index.js
+++ src/index.js
@@ -18,20 +18,19 @@
 // }
 
 function toR (n) {
-    if(!n) {return console.log('Enter a number >0 and <1000')}
     let str = n.toString();
     let newStr = '';
     let arr = [];
     let arr1 = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
     let arr2 = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
     let arr3 = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-    let arr4 = ['one hundred', 'two hundred', 'two hundred', 'three hundred', 'four hundred', 'five hundred', 'six hundred', 'seven hundred', 'eight hundred', 'nine hundred'];
+    let arr4 = ['one hundred', 'two hundred', 'three hundred', 'four hundred', 'five hundred', 'six hundred', 'seven hundred', 'eight hundred', 'nine hundred'];
     console.log(str.length);
 
     switch (str.length) {
         case 1 : {
                 switch (n) {
-                    case 0 : return 'zero';
+                    case 0 : return 'zero'; 
                     case 1 : return 'one';
                     case 2 : return 'two';
                     case 3 : return 'three';
@@ -42,8 +41,7 @@ function toR (n) {
                     case 8 : return 'eight';
                     case 9 : return 'nine';
                 }
-            break;    
-            }; 
+            }; break; 
         case 2 : {
             if (n < 20) {
                 switch (n) {
@@ -70,7 +68,7 @@ function toR (n) {
                 case '9' : newStr += arr3[7] + ' '; break;                
             };
             switch (str[1]){
-                case '0' : newStr += arr1[0]; break;
+                case '0' : newStr = newStr.slice(0, -1); break;
                 case '1' : newStr += arr1[1]; break;
                 case '2' : newStr += arr1[2]; break;
                 case '3' : newStr += arr1[3]; break;
@@ -82,10 +80,74 @@ function toR (n) {
                 case '9' : newStr += arr1[9]; break;
             };
             return newStr;
-        };
+        }; 
+        case 3 : {
+            switch (str[0]){
+                case '1' : newStr += arr4[0] + ' '; break;
+                case '2' : newStr += arr4[1] + ' '; break;
+                case '3' : newStr += arr4[2] + ' '; break;
+                case '4' : newStr += arr4[3] + ' '; break;
+                case '5' : newStr += arr4[4] + ' '; break;
+                case '6' : newStr += arr4[5] + ' '; break;
+                case '7' : newStr += arr4[6] + ' '; break;
+                case '8' : newStr += arr4[7] + ' '; break;
+                case '9' : newStr += arr4[8] + ' '; break;
+            };
+            switch (str[1]){
+                case '0' : {
+                    switch (str[2]){
+                        case '1' : newStr += arr1[1]; return newStr;
+                        case '2' : newStr += arr1[2]; return newStr;
+                        case '3' : newStr += arr1[3]; return newStr;
+                        case '4' : newStr += arr1[4]; return newStr;
+                        case '5' : newStr += arr1[5]; return newStr;
+                        case '6' : newStr += arr1[6]; return newStr;
+                        case '7' : newStr += arr1[7]; return newStr;
+                        case '8' : newStr += arr1[8]; return newStr;
+                        case '9' : newStr += arr1[9]; return newStr;
+                    };
+                }; break;
+                case '1' : {
+                    switch (str[2]){
+                        case '0' : newStr += arr2[0]; return newStr;
+                        case '1' : newStr += arr2[1]; return newStr;
+                        case '2' : newStr += arr2[2]; return newStr;
+                        case '3' : newStr += arr2[3]; return newStr;
+                        case '5' : newStr += arr2[5]; return newStr;
+                        case '4' : newStr += arr2[4]; return newStr;
+                        case '6' : newStr += arr2[6]; return newStr;
+                        case '7' : newStr += arr2[7]; return newStr;
+                        case '8' : newStr += arr2[8]; return newStr;
+                        case '9' : newStr += arr2[9]; return newStr;
+                    };
+                }; break;
+                case '2' : newStr += arr3[0] + ' '; break;
+                case '3' : newStr += arr3[1] + ' '; break;
+                case '4' : newStr += arr3[2] + ' '; break;
+                case '5' : newStr += arr3[3] + ' '; break;
+                case '6' : newStr += arr3[4] + ' '; break;
+                case '7' : newStr += arr3[5] + ' '; break;
+                case '8' : newStr += arr3[6] + ' '; break;
+                case '9' : newStr += arr3[7] + ' '; break;
+            };
+            switch (str[2]){
+                case '0' : newStr = newStr.slice(0, -1); break;
+                case '1' : newStr += arr1[1]; break;
+                case '2' : newStr += arr1[2]; break;
+                case '3' : newStr += arr1[3]; break;
+                case '4' : newStr += arr1[4]; break;
+                case '5' : newStr += arr1[5]; break;
+                case '6' : newStr += arr1[6]; break;
+                case '7' : newStr += arr1[7]; break;
+                case '8' : newStr += arr1[8]; break;
+                case '9' : newStr += arr1[9]; break;
+            };
+            return newStr;
+
+        }; 
         default : 'Nothing to return'
     }
     
 }
 
-console.log(toR(34));
+console.log(toR(199));
